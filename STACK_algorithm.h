ab87da001c2fc13c70c3761a1163da3a0b0ccdcd
Muhammad Habash <muhammad.aly.habash@gmail.com>
2021-12-27 16:18:26 +0200

    Version 1.0

---
+++ STACK_algorithm.h
@@ -0,0 +1,70 @@
+/**************************************************************/
+/*                 Author    : Muhammad Ali Habash            */
+/*                 Version   : 1.0                            */
+/*                 Date      : 12/26/2021                     */
+/*                 Last Edit : N/A                            */
+/**************************************************************/
+
+#ifndef __STACK_ALGORITHM_H__
+#define __STACK_ALGORITHM_H__
+
+/**************************************************************/
+/*                                                            */
+/*                    Files Inclusions                        */
+/*                                                            */
+/**************************************************************/
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+#include "STD_types.h"
+
+/**************************************************************/
+/*                                                            */
+/*                    End Of Section                          */
+/*                                                            */
+/**************************************************************/
+
+
+/**************************************************************/
+/*                                                            */
+/*                    Structs Definitions                     */
+/*                                                            */
+/**************************************************************/
+
+typedef struct gstrNode_t
+{
+	uint8_t u8_Data;
+	struct  gstrNode_t* pstr_next;
+}gstrNode_t;
+
+/**************************************************************/
+/*                                                            */
+/*                    End Of Section                          */
+/*                                                            */
+/**************************************************************/
+
+
+/**************************************************************/
+/*                                                            */
+/*                    Functions Protoypes                     */
+/*                                                            */
+/**************************************************************/
+
+uint8_t check_input_is_digit (uint8_t* u8_line);               // This function is used to check whether input consists only of digits or not
+uint16_t extract_numbers (uint8_t* u8_line);                   // This function converts a number from string format to integer format
+uint16_t select_stack_size ();                                 // This function selects the size of the stack
+void push (uint8_t u8_data);                                   // This function pushes data to the stack by means of a linked list
+uint8_t pull ();                                               // This function pulls data from the stack by means of a linked list
+void printStack();                                             // This function prints the current stack
+uint8_t balancedParentheses(uint8_t* expression);              // This function checks whether the expression passed to it is balanced or not, this is achieved by popping each opening and closing pair, if the stack is empty then the expression is balanced. otherwise, not balanced.
+
+/**************************************************************/
+/*                                                            */
+/*                    End Of Section                          */
+/*                                                            */
+/**************************************************************/
+
+
+#endif
---
+++ stack_algorithm.c
@@ -0,0 +1,242 @@
+/**************************************************************/
+/*                 Author    : Muhammad Ali Habash            */
+/*                 Version   : 1.0                            */
+/*                 Date      : 12/26/2021                     */
+/*                 Last Edit : N/A                            */
+/**************************************************************/
+
+
+/**************************************************************/
+/*                                                            */
+/*                    Files Inclusions                        */
+/*                                                            */
+/**************************************************************/
+
+#include "STACK_algorithm.h"
+
+/**************************************************************/
+/*                                                            */
+/*                    End Of Section                          */
+/*                                                            */
+/**************************************************************/
+
+
+/**************************************************************/
+/*                                                            */
+/*                    Macros                                  */
+/*                                                            */
+/**************************************************************/
+
+#define TRUE  1
+#define FALSE 0
+
+/**************************************************************/
+/*                                                            */
+/*                    End Of Section                          */
+/*                                                            */
+/**************************************************************/
+
+
+/**************************************************************/
+/*                                                            */
+/*                    Global Variables                        */
+/*                                                            */
+/**************************************************************/
+
+int8_t          gs8_Top = -1;                                  // This is the top of the stack
+gstrNode_t*  gpstr_Head = NULL;                                // This is the head of the linked list
+uint16_t gu16_StackSize = 0;                                   // This is the size of the stack before changing it to a desired size
+
+/**************************************************************/
+/*                                                            */
+/*                    End Of Section                          */
+/*                                                            */
+/**************************************************************/
+
+
+/**************************************************************/
+/*                                                            */
+/*                    Functions Definitions                   */
+/*                                                            */
+/**************************************************************/
+
+/******************************************************************************************/
+/* Refer to the STACK_algorithm.h file to read the explanation of each function if needed */
+/******************************************************************************************/
+
+uint8_t check_input_is_digit (uint8_t* u8_Line)  
+{
+	uint32_t u32_LiteralIndex;
+	
+	for(u32_LiteralIndex = 0 ; u32_LiteralIndex < strlen(u8_Line)-1 ; u32_LiteralIndex++)
+	{
+		if ( !( ( *(u8_Line+u32_LiteralIndex) >='0')	&& ( *(u8_Line+u32_LiteralIndex) <= '9' ) ) )
+		{
+			return 0;
+		}
+	}
+	
+    return 1;
+}
+
+uint16_t extract_numbers (uint8_t* u8_Line)
+{
+	uint8_t u8_Index, u8_Number, u8_Weight;
+	uint16_t u16_Sum = 0;
+	u8_Weight        = strlen(u8_Line) - 2;
+	for(u8_Index = 0 ; u8_Index < strlen(u8_Line) - 1 ; u8_Index++)
+	{
+	    u8_Number = ( (u8_Line[u8_Index] - 48 ) );
+	    u16_Sum += ( u8_Number * pow(10, u8_Weight) ); 
+		u8_Weight --;
+    }
+	return u16_Sum;
+}
+
+uint16_t select_stack_size ()
+{
+	uint8_t  u8_Line[256];
+	uint8_t  u8_IsDigit;
+	uint8_t  u8_NoChoice = TRUE;
+	uint16_t u16_StackSize;
+	
+	while (u8_NoChoice)
+	{
+		printf("Choose stack size: ");
+		fflush(stdin);
+		fgets(u8_Line, sizeof(u8_Line), stdin);
+		u8_IsDigit = check_input_is_digit(u8_Line);
+		
+		if(u8_IsDigit)
+		{
+			u8_NoChoice = FALSE;
+			u16_StackSize = extract_numbers(u8_Line);
+		}
+		else 
+			printf("\nWrong choice\n");
+	}
+	return u16_StackSize;
+}
+
+void push (uint8_t u8_data)
+{
+	gstrNode_t* pstr_Temp  = malloc(sizeof(gstrNode_t));
+	pstr_Temp -> u8_Data   = u8_data;
+	pstr_Temp -> pstr_next = NULL;
+	
+	if (gs8_Top < gu16_StackSize)
+	{
+		gs8_Top++;
+	}
+	if (gs8_Top == 0)
+	{
+		gpstr_Head = pstr_Temp;
+	}
+	else if (gs8_Top < gu16_StackSize)
+	{
+		gstrNode_t* pstr_Ptr = gpstr_Head;
+		while (pstr_Ptr -> pstr_next != NULL)
+		{
+			pstr_Ptr = pstr_Ptr -> pstr_next;
+		}
+		pstr_Ptr -> pstr_next = pstr_Temp;
+	}
+	else 
+	{
+		printf("\nStack overflow\n");
+	}
+}
+
+uint8_t pull ()
+{
+	gstrNode_t* pstr_Ptr;
+	uint8_t pop_data;
+	if (gs8_Top == 0)
+	{
+		pop_data = gpstr_Head -> u8_Data;
+		free(gpstr_Head);
+		gpstr_Head = NULL;
+	}
+	else if (gs8_Top > 0)
+	{
+		gstrNode_t* pstr_Ptr = gpstr_Head;
+		while ( ( (pstr_Ptr -> pstr_next) -> pstr_next ) != NULL )
+		{
+			pstr_Ptr = pstr_Ptr -> pstr_next;
+		}
+		pop_data = (pstr_Ptr -> pstr_next)-> u8_Data;
+		free(pstr_Ptr -> pstr_next);
+		pstr_Ptr -> pstr_next = NULL;
+	}
+	else 
+	{
+		printf("\nEmpty Stack\n");
+	}
+	if (gs8_Top >= 0)
+	{
+		gs8_Top--;
+	}		
+	return pop_data;
+}
+
+void printStack()
+{
+	gstrNode_t* pstr_Ptr = gpstr_Head;
+	while (pstr_Ptr != NULL)
+	{
+		printf("%c\n",pstr_Ptr->u8_Data);
+		pstr_Ptr = pstr_Ptr -> pstr_next;
+	}
+}
+
+uint8_t balancedParentheses(uint8_t* expression)
+{
+	uint8_t u8_Index;
+	for (u8_Index = 0 ; u8_Index < strlen(expression) - 1 ; u8_Index++)
+	{
+		if( ( expression[u8_Index] == '{' ) || ( expression[u8_Index] == '(' ) || ( expression[u8_Index] == '[' ) )
+		{
+			push(expression[u8_Index]);
+		}
+		else if( ( expression[u8_Index] == '}' ) ||  ( expression[u8_Index] == ')' ) || ( expression[u8_Index] == ']') )
+		{
+			gstrNode_t* pstr_Ptr = gpstr_Head;
+			if(pstr_Ptr != NULL)
+			{
+		        while(pstr_Ptr->pstr_next != NULL)
+		        {
+			        pstr_Ptr = pstr_Ptr->pstr_next;
+		        }
+			    if((((pstr_Ptr->u8_Data) == '{') && (expression[u8_Index] == '}')) || (((pstr_Ptr->u8_Data) == '[') && (expression[u8_Index] == ']')) || (((pstr_Ptr->u8_Data) == '(') && (expression[u8_Index] == ')')))
+		        {
+			        pull();
+		        }
+			}
+		}
+	}
+	if(gpstr_Head == NULL)
+	{
+		printf("\nParenthesis are balanced\n");
+	}
+	else 
+	{
+		printf("\nParenthesis are not balanced\n");
+	}
+}
+
+/**************************************************************/
+/*                                                            */
+/*                    End Of Section                          */
+/*                                                            */
+/**************************************************************/
+
+uint8_t main()
+{	
+	uint8_t u8_Line[256];
+	gu16_StackSize  = select_stack_size();
+	printf("\nEnter a string: ");
+	fgets(u8_Line,sizeof(u8_Line),stdin);
+	balancedParentheses(u8_Line);
+
+	return 0;
+}
